b3ae0dc2e92946f97d7ef4f7afb7eef073e9639b
HariJW <harshwaychal936@gmail.com>
2025-08-10 01:48:28 +0530

    css : footer styles update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 p-3 fixed left-0 right-0 bottom-0 text-center">
+    <footer className="bg-gray-100 border-t p-3 fixed left-0 right-0 bottom-0 text-center text-sm text-gray-600">
       © {new Date().getFullYear()} Library Dashboard
     </footer>
-  )
+  );
 }
