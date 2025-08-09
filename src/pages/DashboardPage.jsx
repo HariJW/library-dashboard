96e65b03915678b27623588c22c890414eebe268
HariJW <harshwaychal936@gmail.com>
2025-08-10 00:35:31 +0530

    feat : added page

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,8 @@
+import DynamicComponentLoader from '../components/DynamicComponentLoader'
+export default function DashboardPage() {
+  return (
+    <div>
+      <DynamicComponentLoader />
+    </div>
+  )
+}
